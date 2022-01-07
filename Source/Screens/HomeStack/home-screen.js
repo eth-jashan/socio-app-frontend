e23f7200b31912465f3e6febaae0967c026dbed5
[Saahith Shetty] <[shettysaahith123@gmail.com]>
2022-01-08 01:18:02 +0530

    Added PostCard#2

--- Source/Screens/HomeStack/home-screen.js
+++ Source/Screens/HomeStack/home-screen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
+import PostCard from "../../Component/Common/postCard";
 
 const Home_screen = (props) => {
   return <View></View>;
